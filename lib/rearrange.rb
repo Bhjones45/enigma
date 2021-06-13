44262c2d1f79e0a2073f22fca6db6e6c703c82e0
Brett Jones <bhjones4545@gmail.com>
2021-06-13 15:00:32 -0600

    Create and add alphabet method

--- lib/rearrange.rb
+++ lib/rearrange.rb
@@ -1,4 +1,15 @@
 class Rearrange
-  def initialize()
+  attr_reader :message,
+              :key,
+              :date
+
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+  end
+
+  def alphabet
+    ("a".."z").to_a << " "
   end
 end
--- spec/rearrange_spec.rb
+++ spec/rearrange_spec.rb
@@ -7,7 +7,25 @@ RSpec.describe Rearrange do
     it 'exists' do
       rearrange = Rearrange.new("hello world", "02715", "040895")
 
-      expect(rearrange).to eq(Rearrange)
+      expect(rearrange).to be_a(Rearrange)
+    end
+
+    it 'has attributes' do
+      rearrange = Rearrange.new("hello world", "02715", "040895")
+
+      expect(rearrange.message).to eq("hello world")
+      expect(rearrange.key).to eq("02715")
+      expect(rearrange.date).to eq("040895")
+    end
+  end
+
+  describe 'Methods' do
+    it 'can create alphabet array' do
+      rearrange = Rearrange.new("hello world", "02715", "040895")
+      
+      expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+      expect(rearrange.alphabet).to eq(expected)
     end
   end
 end
