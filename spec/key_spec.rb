fa47e62c925c8dd5b523c353425f1f2890f9e105
Brett Jones <bhjones4545@gmail.com>
2021-06-13 13:32:42 -0600

    Create key tests #offsets

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -15,5 +15,23 @@ RSpec.describe Key do
 
       expect(@key.input_key).to eq([0, 2, 7, 1, 5])
     end
+
+    it 'can take the date and square it' do
+      @key = Key.new('02715', '040895')
+
+      expect(@key.date_squared).to eq(1672401025)
+    end
+
+    it 'can turn date squared into string' do
+      @key = Key.new('02715', '040895')
+
+      expect(@key.date_squared_string).to eq('1672401025')
+    end
+
+    it 'can take last 4 digits from date squared string' do
+      @key = Key.new('02715', '040895')
+
+      expect(@key.new_offset).to eq(1025)
+    end
   end
 end
