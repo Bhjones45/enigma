dc1e5dc722fa953dbfc04fd2417f071f0af29e0d
Brett Jones <bhjones4545@gmail.com>
2021-06-13 13:22:02 -0600

    Create input key test

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe Key do
       expect(@key).to be_a(Key)
     end
   end
+
+  describe 'Methods' do
+    it 'can return an array from a string' do
+      @key = Key.new('02715', '040895')
+
+      expect(@key.input_key).to eq([0, 2, 7, 1, 5])
+    end
+  end
 end
