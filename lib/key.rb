b8332a2c5aa2be6def2a899247cb7cb64f77262f
Brett Jones <bhjones4545@gmail.com>
2021-06-13 13:32:58 -0600

    Add methods passing except new offset

--- lib/key.rb
+++ lib/key.rb
@@ -9,4 +9,18 @@ class Key
       key.to_i
     end
   end
+
+  def date_squared
+    @offset.to_i ** 2
+  end
+
+  def date_squared_string
+    date_squared.to_s
+  end
+
+  def new_offset
+    date_squared_string.split('').map do |offset|
+      offset.to_i
+    end
+  end
 end
