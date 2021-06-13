776713d7c7863747282a6022e576e06b3c10d8cd
Brett Jones <bhjones4545@gmail.com>
2021-06-13 13:09:18 -0600

    File creation

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,9 +3,14 @@ require 'date'
 require './lib/enigma'
 
 RSpec.describe Enigma do
-  describe 'instantiation' do
+  describe 'Instantiation' do
     it 'exists' do
       enigma = Enigma.new
+
+      expect(enigma).to be_a(Enigma)
     end
   end
+
+  describe 'Methods' do
+    
 end
