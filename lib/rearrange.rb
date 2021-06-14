892388a1922dd710ef3c5eab8dbab2ed6a2193ae
Brett Jones <bhjones4545@gmail.com>
2021-06-13 20:24:01 -0600

    Add character index method with pass

--- lib/rearrange.rb
+++ lib/rearrange.rb
@@ -12,4 +12,17 @@ class Rearrange
   def alphabet
     ("a".."z").to_a << " "
   end
+
+  def character_index(num)
+    message_string = @message.split('')
+    characters = message_string.each_with_index.map do |character, index|
+      if index % 4 == num
+        character
+      end
+    end
+    characters.compact
+  end
+
+  # def rearranged_encrypted_a
+  #   character_index
 end
